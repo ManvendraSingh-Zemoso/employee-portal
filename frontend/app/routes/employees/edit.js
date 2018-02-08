d5b99322d646687ea12e995b3fa80909320b2125
Manvendra Singh <manvendrasingh.narwar@zemosolabs.com>
2018-02-08 15:36:30 +0530

    added untracked files

---
+++ frontend/app/routes/employees/edit.js
@@ -0,0 +1,55 @@
+import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
+
+export default Route.extend({
+  model(param){
+    return RSVP.hash({
+      employee: this.get('store').findRecord('employee',param.id),
+      picture: this.get('store').createRecord('picture'),
+      reportToEmployees: this.get('store').findAll('employee'),
+      departments: this.get('store').findAll('department'),
+      projects: this.get('store').findAll('project'),
+      roles: this.get('store').findAll('role'),
+      locations: this.get('store').findAll('location'),
+      skills: this.get('store').findAll('skill')
+    });
+  },
+  setupController(controller, model) {
+    this._super(controller, model);
+    controller.set('employee', model.employee);
+    let employeeList = [];
+    model.reportToEmployees.forEach(function (reportToEmployee) {
+      if(reportToEmployee.get('firstName')!=null){
+        if(reportToEmployee.get('firstName')!=model.employee.get('firstName')){
+          employeeList.push(reportToEmployee.get('firstName'));
+        }
+      }
+    });
+    controller.set('employeeList', employeeList);
+    let departmentList = [];
+    model.departments.forEach(function (department) {
+      departmentList.push(department.get('name'))
+    });
+    controller.set('departmentList',departmentList);
+    let projectList = [];
+    model.projects.forEach(function (project) {
+      projectList.push(project.get('name'))
+    });
+    controller.set('projectList',projectList);
+    let roleList = [];
+    model.roles.forEach(function (role) {
+      roleList.push(role.get('name'))
+    });
+    controller.set('roleList',roleList);
+    let locationList = [];
+    model.locations.forEach(function (location) {
+      locationList.push(location.get('name'))
+    });
+    controller.set('locationList',locationList);
+    let skillList = [];
+    model.skills.forEach(function (skill) {
+      skillList.push(skill.get('name'))
+    });
+    controller.set('skillList',skillList);
+  }
+});
